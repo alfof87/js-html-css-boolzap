ca1c0b15676d888c1e08f9c975973828624b78b8
Alfonso <alfof87@gmail.com>
2020-07-14 15:04:30 +0200

    js-html-css-boolzapp prova nuova

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-function inserimentoMsg(){
-  $("button").click(function() {
-    var myClone = $("#write input").clone();
-    $("#colonna-msg-utente").append(myClone);
-  });
+
+
+
+function addSendListener(){
+  var target = $("#new-message-input");
+  target.keyup(sendKeyup);
 }
+function sendKeyup(event){
+
+
+  var key = event.wich;
+  console.log(key);
+  if (key === 13) {
+    var input = $(this);
+    var txt = input.val();
+    input.val(" ");
+    sendMessage(txt);
+  }
+
+}
+
+function sendMessage(txt){
+  var template = $(".template-message-sent").clone();
+  var target = $("#colonna-msg-utente");
+  template.find("#new-message-input").text(txt);
+  target.append(template);
+  var msg = $(".msg-utente");
+  msg.addClass(".template-message-sent");
+}
+
+
+
+
+// function inserimentoMsg(){
+//   $("button").click(function() {
+//     var myClone = $("#write input").clone();
+//     $("#colonna-msg-utente").append(myClone);
+//   });
+// }
 // function inserimentoMsg(){
 //   $("button").click(function() {
 //     var myClone = $("#write input").text();
@@ -12,12 +45,7 @@ function inserimentoMsg(){
 //   });
 // }
 
-
-
-
-
-
 function init(){
-inserimentoMsg();
+addSendListener();
 }
 $(document).ready(init);
